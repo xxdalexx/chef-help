be543b59e625fd771ce4b02a3f87da74ad06668f
Dale Carter <xthedalex@gmail.com>
2022-04-10 15:35:30 -0400

    New: Factory Macros
    Macros for creating a fake price and selecting a fake unit from measurement enums. Registered in AppServiceProvider.php.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Measurements\UsVolume;
+use App\Measurements\UsWeight;
+use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +26,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Factory::macro('fakePrice', function () {
+            $first = (string) $this->faker->randomDigit();
+            $second = (string) $this->faker->numberBetween(10,99);
+            return $first .".". $second;
+        });
+
+        Factory::macro('fakeUnit', function () {
+            return collect(UsWeight::cases())
+                ->merge(UsVolume::cases())
+                ->pluck('value')
+                ->random();
+        });
     }
 }
