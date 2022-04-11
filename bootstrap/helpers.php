d06fc2b9fe52a2816d374b32ac945593e8fd6192
Dale Carter <xthedalex@gmail.com>
2022-04-11 08:10:39 -0400

    Update(Docs): PHPDoc Blocks on Helper Functions.

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -11,7 +11,17 @@ use Brick\Money\Context\CustomContext;
 use Brick\Money\Money;
 use Illuminate\Support\Str;
 
-function moneyToString(Money $money, $withDollarSign = true): string
+
+/**
+ * A function is required here because the money class from the Brick\Money\Money package is a final class, so it can't
+ * be extended to add more methods.
+ *
+ * @param Money $money
+ * @param bool  $withDollarSign
+ *
+ * @return string
+ */
+function moneyToString(Money $money, bool $withDollarSign = true): string
 {
     $string = $money->to(new CustomContext(2), RoundingMode::UP)->formatTo('en_US');
 
@@ -22,7 +32,17 @@ function moneyToString(Money $money, $withDollarSign = true): string
     }
 }
 
-function money($price, $decimalPlaces = 4): Money
+
+/**
+ * Shortcut function for creating a new Money object.
+ *
+ * @param     $price
+ * @param int $decimalPlaces
+ *
+ * @return Money
+ * @throws \Brick\Money\Exception\UnknownCurrencyException
+ */
+function money($price, int $decimalPlaces = 4): Money
 {
     return Money::of($price, 'USD', new CustomContext($decimalPlaces), RoundingMode::UP);
 }
