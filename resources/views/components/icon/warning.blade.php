19ed1ce078e5572b4041dd678bca199c4d26f6f5
Dale Carter <xthedalex@gmail.com>
2022-04-20 10:21:56 -0400

    Fix: Tooltips
    Add wire ignore to warning icon component so tooltips don't lose functionality when a livewire component re-renders.

--- resources/views/components/icon/warning.blade.php
+++ resources/views/components/icon/warning.blade.php
@@ -1,4 +1,7 @@
-<i class="bi bi-exclamation-circle text-warning"
-   data-bs-toggle="tooltip"
-   data-bs-placement="top"
-   title="{{ $tooltip }}"></i>
+<span wire:ignore>
+    <i class="bi bi-exclamation-circle text-warning"
+       data-bs-toggle="tooltip"
+       data-bs-placement="top"
+       title="{{ $tooltip }}"></i>
+</span>
+
