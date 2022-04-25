aa0e627f12c58d2d9a0c5c64b89bf0f72198afaf
Dale Carter <xthedalex@gmail.com>
2022-04-25 16:20:18 -0400

    Update(Design): Conditional Edit/Close buttons on x-card
    Add ability to conditionally show:
    - Edit icon when edit-click attribute present
    - Close icon when close-click attribute present
    
    Attribute takes livewire function to call on click as value.

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,5 +1,13 @@
 <div class="card {{ $cardClass ?? '' }}">
-    <h5 class="card-header {{ $headerClass ?? '' }}">{{ $title }}</h5>
+    <h5 class="card-header {{ $headerClass ?? '' }} d-flex justify-content-between">
+        {{ $title }}
+        @if($attributes->has('edit-click'))
+            <i wire:click="{{ $attributes->get('edit-click') }}" class="bi bi-pencil-square cursor-pointer"></i>
+        @endif
+        @if($attributes->has('close-click'))
+            <i wire:click="{{ $attributes->get('close-click') }}" class="bi bi-x-lg cursor-pointer"></i>
+        @endif
+    </h5>
     <div class="card-body">
         {{ $slot }}
     </div>
